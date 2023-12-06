ee9db28d61476f733612809c71f5178d0b311126
DaMelonnPawahh <106388062+TheMelonnn@users.noreply.github.com>
2023-12-06 13:32:07 +0700

    reset

--- Progress Mobile Development/MD-main/app/src/main/java/com/example/capstone/ui/navigation/NavigationItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.capstone.ui.navigation
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavigationItem(
-    val title: String,
-    val image: ImageVector,
-    val screen: Screen
-)
